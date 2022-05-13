c9a2b4495ae8fa2e3c8ff93fd0657c6f4efc315f
AldoSamaniego <a00825779@itesm.mx>
2022-05-12 21:12:59 -0500

    avan ce front end 2do parcial

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const app=Vue.createApp({
+    data(){
+        return{
+            addi : false,
+            timeline : true,
+            friends : false,
+            campos : ['Mapa','Logo','Mejor Atractivo'],
+            campo : ""
+        }
+    }
 
 })
 app.component('footer-component',{
@@ -36,6 +45,132 @@ app.component('footer-component',{
     <hr />
     <p class="fs-small">recipy &copy; 2022 Desarrollo de Aplicaciones WEB</p>
     </div>
+    `
+})
+
+app.component('header-component',{
+    props:{
+        active1: String,
+        active2: String,
+        active3: String
+    },
+    template: `
+    <div class="bg-white d-flex align-items-center fixed-top shadow" style="min-height: 56px; z-index:5;">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col d-flex align-items-center">
+                <!--logo-->
+                <i class="fa-solid fa-utensils d-lg-none  text-danger" style="font-size: 1.6rem"></i>
+                <h1 class="text-danger fw-bold d-none d-lg-block" style="font-size: 2rem">recipy</h1>
+                <!--search-->
+                <div class="input-group ms-2">
+                    <span class="input-group-prepend  d-lg-none"
+                    id="searchMenu" 
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                    type="button">
+                        <div class="input-group-text bg-gray border-0 rounded-circle" style="min-height: 40px;">
+                            <i class="fas fa-search text-muted"></i>
+
+                        </div>
+                    </span>
+                    <span class="input-group-prepend d-none d-lg-block"
+                    id="searchMenu" 
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                    type="button">
+                        <div class="input-group-text bg-gray border-0 rounded-pill" style="min-height: 40px; min-width: 230px;">
+                            <i class="fas fa-search me-2 text-muted"></i>
+                            <p class="m-0 fs-small text-muted">Search</p>
+
+                        </div>
+                    </span>
+
+                    <ul class="dropdown-menu border-0 shadow p-3 overflow-auto" aria-labelledby="searchMenu">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+
+                </div>
+            </div>
+            <!--center-->
+            <div class="col d-flex justify-content-center">
+                <div class="nav__btn "  :class="active1">
+                    <a href="./feed.html">
+                        <button class="btn">
+                            <i class="fa-solid fa-house text-danger fs-footer"style="font-size: 1.6rem"></i>
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <!--right-->
+            <div class="col d-flex align-items-center justify-content-end">
+                <div class="nav__btn" :class="active2">
+                    <a href="./profile.html">
+                        <button class="btn ">
+                            <i class="fa-solid fa-user text-danger fs-footer"style="font-size: 1.6rem"></i>
+                        </button>
+                    </a>
+                </div>
+                <div class="nav__btn" :class="active3">
+                    <a href="./settings.html">
+                        <button class="btn ">
+                            <i class="fa-solid fa-bars text-danger fs-footer"style="font-size: 1.6rem"></i>
+                        </button>
+                    </a>
+                </div>
+            </div>
+
+
+
+        </div>
+
+    </div>
+</div>
+    `
+})
+app.component('header2-component',{
+    template: `
+    <div class="bg-danger d-flex align-items-center fixed-buttom " style="min-height: 30px; z-index:5;">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col d-flex justify-content-center">
+                <div class="nav__btn nav__btn-active2">
+                    <a href="./feed.html">
+                        <button class="btn">
+                            <i class="fa-solid fa-circle-plus text-white fs-footer"style="font-size: 1.3rem"></i>
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <!--center-->
+            <div class="col d-flex justify-content-center">
+                <div class="nav__btn nav__btn-active2">
+                    <a href="./feed.html">
+                        <button class="btn">
+                            <i class="fa-solid fa-image-portrait text-white fs-footer"style="font-size: 1.3rem"></i>
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <!--right-->
+            <div class="col d-flex align-items-center justify-content-end">
+                <div class="nav__btn">
+                    <a href="./settings.html">
+                        <button class="btn ">
+                            <i class="fa-solid fa-user-group text-white fs-footer"style="font-size: 1.3rem"></i>
+                        </button>
+                    </a>
+                </div>
+            </div>
+
+
+
+        </div>
+
+    </div>
+</div>
     `
 })
 
